3f6657534cbd8e279276d16cd1cceea908a6dd9d
ArtemLipatov <houndvibe@gmail.com>
2020-11-30 23:23:50 +0700

    action creators reworked

--- src/store/actions.js
+++ src/store/actions.js
@@ -12,76 +12,150 @@ import {
   ON_SET,
 } from "./actionTypes";
 
-export function onSort(type) {
-  return {
-    type: ON_SORT,
-    payload: type,
-  };
-}
-export function onFilter(value) {
-  return {
+export const onFilter = (type) => (dispatch) => {
+  dispatch({
     type: ON_FILTER,
-    payload: value,
-  };
-}
-export function onTypeChange(type, index) {
-  return {
+    payload: type,
+  });
+};
+export const onSort = (value) => (dispatch, getState) => {
+  let sortedState = { ...getState() };
+  if (value === "distance") {
+    sortedState.workoutsList.sort((a, b) => b.wDistance - a.wDistance);
+    //сортируем список тренировок по длинне комментария
+  } else if (value === "comment") {
+    sortedState.workoutsList.sort((a, b) =>
+      a.wComment.length < b.wComment.length ? 1 : -1
+    );
+    //сортируем список тренировок по типу
+  } else if (value === "type") {
+    sortedState.workoutsList.sort((a, b) => (a.wType < b.wType ? 1 : -1));
+    //сортируем список тренировок по дате
+  } else if (value === "date") {
+    sortedState.workoutsList.sort((a, b) => (+a.wDate > +b.wDate ? 1 : -1));
+    //сортируем список тренировок по индексу
+  } else if (value === "index") {
+    sortedState = { ...getState() };
+  }
+  dispatch({
+    type: ON_SORT,
+    payload: sortedState,
+  });
+};
+
+export const onTypeChange = (type, index) => (dispatch, getState) => {
+  let newState = { ...getState() };
+  newState.workoutsList[index].wType = type;
+  dispatch({
     type: ON_TYPE_CHANGE,
-    payload: type,
-    payload2: index,
-  };
-}
-export function onCommentChange(comment, index) {
-  return {
+    payload: newState,
+  });
+};
+
+export const onCommentChange = (comment, index) => (dispatch, getState) => {
+  let newCommentState = { ...getState() };
+  newCommentState.workoutsList[index].wComment = comment;
+  dispatch({
     type: ON_COMMENT_CHANGE,
-    payload: comment,
-    payload2: index,
-  };
-}
-export function onDistanceChange(value, index) {
-  return {
-    type: ON_DISTANCE_CHANGE,
-    payload: value,
-    payload2: index,
-  };
-}
-export function onDateChange(date, index, position) {
-  return {
+    payload: newCommentState,
+  });
+};
+
+export const onDistanceChange = (value, index) => (dispatch, getState) => {
+  //регулярка для валидации инпута ввода дистанции, пропускающая только цифры
+  let regEx = /^\d*\.?\d*$/;
+
+  if (!value.match(regEx)) {
+    return;
+  } else {
+    let newDistanceState = { ...getState() };
+    newDistanceState.workoutsList[index].wDistance = value;
+
+    dispatch({
+      type: ON_DISTANCE_CHANGE,
+      payload: newDistanceState,
+    });
+  }
+};
+
+export const onDateChange = (newDate, index, position) => (
+  dispatch,
+  getState
+) => {
+  let newDateState = { ...getState() };
+  let date = new Date(+newDateState.workoutsList[index].wDate * 1000);
+  switch (position) {
+    case "date":
+      date.setFullYear(date.getFullYear(), date.getMonth(), newDate);
+      break;
+    case "month":
+      date.setFullYear(date.getFullYear(), newDate, date.getDate());
+      break;
+    case "year":
+      date.setFullYear(newDate, date.getMonth(), date.getDate());
+      break;
+    default:
+      break;
+  }
+  newDateState.workoutsList[index].wDate = date.getTime() / 1000;
+
+  dispatch({
     type: ON_DATE_CHANGE,
-    payload: date,
-    payload2: index,
-    payload3: position,
-  };
-}
-export function onWorkoutAdd(type) {
-  return {
-    type: ON_WORKOUT_ADD,
-    payload: type,
-  };
-}
-export function onRowMouseEnter(index) {
-  return {
+    payload: newDateState,
+  });
+};
+
+export const onWorkoutAdd = (type) => (dispatch, getState) => {
+  if (type === "disabled") {
+    type = "running";
+  }
+
+  let workoutsList = getState().workoutsList.concat({
+    wType: type,
+    wDate: Date.now() / 1000,
+    wDistance: 0,
+    wComment: "...",
+    wId: +getState().workoutsList.length,
+  });
+
+  dispatch({ type: ON_WORKOUT_ADD, payload: workoutsList });
+};
+
+//ON_ROW_MOUSE_ENTER и ON_ROW_MOUSE_LEAVE нужны для отображения кнопки удаления
+//при наведении на нужную тренировку.
+
+export const onRowMouseEnter = (index) => (dispatch, getState) => {
+  let rowMouseEnterState = { ...getState() };
+  rowMouseEnterState.workoutsList[index].Xmark = true;
+  dispatch({
     type: ON_ROW_MOUSE_ENTER,
-    payload: index,
-  };
-}
-export function onRowMouseLeave(index) {
-  return {
+    payload: rowMouseEnterState,
+  });
+};
+
+export const onRowMouseLeave = (index) => (dispatch, getState) => {
+  let rowMouseLeaveState = { ...getState() };
+  rowMouseLeaveState.workoutsList[index].Xmark = false;
+  dispatch({
     type: ON_ROW_MOUSE_LEAVE,
-    payload: index,
-  };
-}
-export function onDeleteHandler(index) {
-  return {
+    payload: rowMouseLeaveState,
+  });
+};
+
+export const onDeleteHandler = (index) => (dispatch, getState) => {
+  let afterDeleteState = { ...getState() };
+  afterDeleteState.workoutsList.splice(index, 1);
+  dispatch({
     type: ON_DELETE_HANDLER,
-    payload: index,
-  };
-}
-export function asyncSetState(data) {
+    payload: afterDeleteState,
+  });
+};
+
+export const asyncSetState = (data) => {
   return (dispatch) => {
     dispatch({
       type: ON_SET,
       payload: data,
     });
   };
-}
+};
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -19,131 +19,54 @@ const initialState = {
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
-    case ON_SORT:
-      //сортируем список тренировок по дистанции
-      if (action.payload === "distance") {
-        let sortedState = { ...state };
-        sortedState.workoutsList.sort((a, b) => b.wDistance - a.wDistance);
-        return sortedState;
-        //сортируем список тренировок по длинне комментария
-      } else if (action.payload === "comment") {
-        let sortedState = { ...state };
-        sortedState.workoutsList.sort((a, b) =>
-          a.wComment.length < b.wComment.length ? 1 : -1
-        );
-        return sortedState;
-        //сортируем список тренировок по типу
-      } else if (action.payload === "type") {
-        let sortedState = { ...state };
-        sortedState.workoutsList.sort((a, b) => (a.wType < b.wType ? 1 : -1));
-        return sortedState;
-        //сортируем список тренировок по дате
-      } else if (action.payload === "date") {
-        let sortedState = { ...state };
-        sortedState.workoutsList.sort((a, b) => (+a.wDate > +b.wDate ? 1 : -1));
-        return sortedState;
-        //сортируем список тренировок по индексу
-      } else if (action.payload === "index") {
-        return state;
-      }
-      break;
-
     case ON_FILTER:
       return {
         ...state,
         filterBy: action.payload,
       };
 
+    case ON_SORT:
+      return action.payload;
+
     case ON_TYPE_CHANGE:
-      let newState = { ...state };
-      newState.workoutsList[action.payload2].wType = action.payload;
       return {
-        ...newState,
+        ...action.payload,
       };
 
     case ON_COMMENT_CHANGE:
-      let newCommentState = { ...state };
-      newCommentState.workoutsList[action.payload2].wComment = action.payload;
       return {
-        ...newCommentState,
+        ...action.payload,
       };
 
     case ON_DISTANCE_CHANGE:
-      //регулярка для валидации инпута ввода дистанции, пропускающая только цифры
-      let regEx = /^\d*\.?\d*$/;
-
-      if (!action.payload.match(regEx)) {
-        return {
-          ...state,
-        };
-      }
-
-      let newDistanceState = { ...state };
-      newDistanceState.workoutsList[action.payload2].wDistance = action.payload;
       return {
-        ...newDistanceState,
+        ...action.payload,
       };
 
     case ON_DATE_CHANGE:
-      let newDateState = { ...state };
-      let date = new Date(
-        +newDateState.workoutsList[action.payload2].wDate * 1000
-      );
-
-      //обновляем  необходимый элемент даты
-      switch (action.payload3) {
-        case "date":
-          date.setFullYear(date.getFullYear(), date.getMonth(), action.payload);
-          break;
-        case "month":
-          date.setFullYear(date.getFullYear(), action.payload, date.getDate());
-          break;
-        case "year":
-          date.setFullYear(action.payload, date.getMonth(), date.getDate());
-          break;
-        default:
-          break;
-      }
-      newDateState.workoutsList[action.payload2].wDate = date.getTime() / 1000;
       return {
-        ...newDateState,
+        ...action.payload,
       };
 
     case ON_WORKOUT_ADD:
-      if (action.payload === "disabled") {
-        action.payload = "running";
-      }
       return {
         ...state,
-        workoutsList: state.workoutsList.concat({
-          wType: action.payload,
-          wDate: Date.now() / 1000,
-          wDistance: 0,
-          wComment: "...",
-          wId: +state.workoutsList.length,
-        }),
+        workoutsList: action.payload,
       };
-    //ON_ROW_MOUSE_ENTER и ON_ROW_MOUSE_LEAVE нужны для отображения кнопки удаления
-    //при наведении на нужную тренировку.
+
     case ON_ROW_MOUSE_ENTER:
-      let rowMouseEnterState = { ...state };
-      rowMouseEnterState.workoutsList[action.payload].Xmark = true;
       return {
-        ...rowMouseEnterState,
+        ...action.payload,
       };
 
     case ON_ROW_MOUSE_LEAVE:
-      let rowMouseLeaveState = { ...state };
-      rowMouseLeaveState.workoutsList[action.payload].Xmark = false;
       return {
-        ...rowMouseLeaveState,
+        ...action.payload,
       };
 
     case ON_DELETE_HANDLER:
-      let afterDeleteState = { ...state };
-      afterDeleteState.workoutsList.splice(action.payload, 1);
       return {
-        ...afterDeleteState,
+        ...action.payload,
       };
 
     case ON_SET:
